56fb6b788c044b2511e80f960106825a864dc8a6
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-10-20 19:57:15 +0000

    template integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
+$app->usePublicPath($app->basePath());
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +42,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->usePublicPath($app->basePath());
 
 /*
 |--------------------------------------------------------------------------
