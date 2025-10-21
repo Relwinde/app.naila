2b7a705df7fec4e4784f9abfa620bee448be908f
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-10-21 10:04:00 +0000

    setting up menu

---
+++ resources/views/partials/side-overlay.blade.php
@@ -0,0 +1,29 @@
+<aside id="side-overlay">
+            <!-- Side Header -->
+            <div class="content-header border-bottom">
+                <!-- User Avatar -->
+                <a class="img-link mr-1" href="javascript:void(0)">
+                    <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar10.jpg" alt="">
+                </a>
+                <!-- END User Avatar -->
+
+                <!-- User Info -->
+                <div class="ml-2">
+                    <a class="text-dark font-w600 font-size-sm" href="javascript:void(0)">Adam McCoy</a>
+                </div>
+                <!-- END User Info -->
+
+                <!-- Close Side Overlay -->
+                <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                <a class="ml-auto btn btn-sm btn-alt-danger" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_close">
+                    <i class="fa fa-fw fa-times"></i>
+                </a>
+                <!-- END Close Side Overlay -->
+            </div>
+            <!-- END Side Header -->
+
+            <!-- Side Content -->
+            @include('partials.sidebar-right')
+            --- IGNORE ---
+            <!-- END Side Content -->
+    </aside>
