5ad3b81d5f01d652c167c76a452edd982d55ad8c
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-10-21 10:43:49 +0000

    fortify layouts setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\Home;
+use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Home::class)->name('home') ;
-Route::get('/', function () {
-    return view('index');
-});
 
-Route::get('/login', App\Livewire\Login::class)->name('login');
+Route::get('/login', Login::class)->name('login');
