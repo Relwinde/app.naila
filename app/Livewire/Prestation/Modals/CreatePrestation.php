e03b296f5e7c060882339a0cb314bdbbc65e4810
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-10-22 12:16:37 +0000

    starting prestations recording

---
+++ app/Livewire/Prestation/Modals/CreatePrestation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Prestation\Modals;
+
+use Livewire\Component;
+
+class CreatePrestation extends Component
+{
+    public function render()
+    {
+        return view('livewire.prestation.modals.create-prestation');
+    }
+}
---
+++ app/Livewire/Prestation/Prestations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Prestation;
+
+use Livewire\Component;
+
+class Prestations extends Component
+{
+    public function render()
+    {
+        return view('livewire.prestation.prestations');
+    }
+}
